d812c3bdddcac3e10b2169e545908f7e4ad3d513
codingninja2 <edwinafatsawo@gmail.com>
2024-12-14 12:25:29 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Input: Get a number from the user
+number = float(input("Enter a number: "))
+
+# Calculate the square
+square = number ** 2
+
+# Output the result
+print("The square of", number, "is:", square)
